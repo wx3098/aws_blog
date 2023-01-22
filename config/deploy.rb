e075ef20285b7d77892e684bc70747abd9f05d58
kouichi shinkokura <stndard809@gmail.com>
2023-01-23 07:41:20 +0900

    unicornやり直し

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.16.0"
 
-set :application, "achieve"
+set :application, "aws_blog"
 set :repo_url, "https://github.com/wx3098/aws_blog.git"
 
 set :branch, ENV['BRANCH'] || 'master'
